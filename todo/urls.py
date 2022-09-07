67239530a06d7ad36b6c43acba9910dcb6f1a387
toufiqulislam525 <toufiqulislam525@gmail.com>
2022-09-08 03:54:54 +0600

    show_tasks added

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('tasks',views.show_tasks,name = 'show_tasks')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 
 def home(request):
     return render(request, 'home.html')
+
+def show_tasks(request):
+    tasks = Task.objects.all()
+    context= {"tasks" : tasks}
+    return render(request,'tasks.html',context)
