39e7a8b2b0bfa15ec1ebd29a22d8a47848707d16
toufiqulislam525 <toufiqulislam525@gmail.com>
2022-09-08 04:10:59 +0600

    Form Generated

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('tasks',views.show_tasks,name = 'show_tasks')
+    path('tasks',views.show_tasks,name = 'show_tasks'),
+    path('create-task',views.create_task, name = 'create_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Task
-
+from .forms import TaskForm
 
 def home(request):
     return render(request, 'home.html')
@@ -10,3 +9,11 @@ def show_tasks(request):
     tasks = Task.objects.all()
     context= {"tasks" : tasks}
     return render(request,'tasks.html',context)
+
+def create_task(request):
+    form = TaskForm()
+    context = {"form" : form}
+    if(request.POST):
+        return render(request,'home.html')
+    else:
+        return render(request, 'create_task.html',context)
