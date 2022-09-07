6ca5836412b8987ae489c8ebee74334e1ec03740
toufiqulislam525 <toufiqulislam525@gmail.com>
2022-09-08 04:10:41 +0600

    Model Form Added

---
+++ todo/forms.py
@@ -0,0 +1,9 @@
+# Model Forms
+from dataclasses import fields
+from django.forms import ModelForm
+from .models import Task
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
