e064eb1c4495b7d3ed44881548bf2fdf7a4a646e
toufiqulislam525 <toufiqulislam525@gmail.com>
2022-09-08 06:30:15 +0600

    Added date time field to complete

--- todo/forms.py
+++ todo/forms.py
@@ -1,9 +1,15 @@
 # Model Forms
-from dataclasses import fields
+
 from django.forms import ModelForm
+from django import forms
 from .models import Task
+from django.contrib.admin.widgets import AdminDateWidget,AdminSplitDateTime
 
 class TaskForm(ModelForm):
+    completed = forms.SplitDateTimeField(widget=AdminSplitDateTime())
     class Meta:
         model = Task
         fields = '__all__'
+        
+        
+        
--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('tasks',views.show_tasks,name = 'show_tasks'),
-    path('create-task',views.create_task, name = 'create_task')
+    path('tasks/',views.show_tasks,name = 'show_tasks'),
+    path('create-task/',views.create_task, name = 'create_task'),
+    path('jsi18n',JavaScriptCatalog.as_view(),name='js-catlog')
 ]
